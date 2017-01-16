66c3860f2c8fce07c1ce2bc2c82b123dddf4b28e
chris-ying <chrisyy0211@gmail.com>
2017-01-15 20:53:25 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $("#down-btn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about-me").offset().top
+        }, 1000);
+    });
+    
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.toTop').fadeIn();
+		} else {
+			$('.toTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.toTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+    
+});
