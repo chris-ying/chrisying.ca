0f151854dd26ba95d424b1fd894bac913fe9df93
chris-ying <chrisyy0211@gmail.com>
2017-03-02 11:14:53 -0500

    Content Change

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
     
+    new WOW( {mobile: false} ).init(); 
+	
     $("#down-btn").click(function() {
         $('html, body').animate({
-            scrollTop: $("#about-me").offset().top
+            scrollTop: $("#work").offset().top
         }, 1000);
     });
     
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.toTop').fadeIn();
-		} else {
-			$('.toTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.toTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
+    $('.popup-gallery').each(function() { 
+        $(this).magnificPopup({
+            delegate: 'a', 
+            type: 'image',
+            gallery: {
+                enabled: true,
+                preload: [0,2], 
+                navigateByImgClick: true,
+            }
+        });
+    });
     
 });
