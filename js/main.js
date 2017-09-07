f5ee1268d5749beefd2429366c4997e676d560f2
Chris Ying <narrowcontent@Chris-Macbook.local>
2017-09-07 16:30:44 -0400

    Add typing animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    
-    new WOW( {mobile: false} ).init(); 
+	
+		function startTyping() {
+			Typed.new('.typing-1', {
+				strings: ["Born in Shanghai, living in Toronto."],
+				typeSpeed: 30,
+				startDelay: 1500,
+		});
+		Typed.new('.typing-2', {
+				strings: ["I'm a web developer / world traveller."],
+				typeSpeed: 30,
+				startDelay: 4200,
+		});
+	}
+		startTyping();
 	
     $("#down-btn").click(function() {
         $('html, body').animate({
